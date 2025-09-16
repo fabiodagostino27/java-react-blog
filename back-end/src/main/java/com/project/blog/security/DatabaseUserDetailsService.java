ec1495d3517e598fe2f4b96e143aaa27fafc0acf
fabiodagostino27 <fabiodago27@gmail.com>
2025-09-16 16:07:17 +0200

    Implement loadUserByUsername method in DatabaseUserDetailsService for user authentication

--- back-end/src/main/java/com/project/blog/security/DatabaseUserDetailsService.java
+++ back-end/src/main/java/com/project/blog/security/DatabaseUserDetailsService.java
@@ -1,15 +1,26 @@
 package com.project.blog.security;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
+import com.project.blog.model.User;
+import com.project.blog.repo.UserRepository;
+
 public class DatabaseUserDetailsService implements UserDetailsService {
+    @Autowired
+    private UserRepository userRepository;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'loadUserByUsername'");
+        Optional<User> userAttempt = userRepository.findbyUsername(username);
+
+        if (userAttempt.isEmpty()) throw new UsernameNotFoundException("No user found with this username" + username);
+
+        return new DatabaseUserDetails(userAttempt.get());
     }
 
 }
