7bc127bbda3560cfae5cd9868312871f6e8bcbc6
fabiodagostino27 <fabiodago27@gmail.com>
2025-09-16 16:00:57 +0200

    Implement DatabaseUserDetailsService for user authentication and create SecurityConfig class

---
+++ back-end/src/main/java/com/project/blog/security/DatabaseUserDetailsService.java
@@ -0,0 +1,45 @@
+package com.project.blog.security;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.project.blog.model.Role;
+import com.project.blog.model.User;
+
+public class DatabaseUserDetailsService implements UserDetails {
+    private final Integer id;
+    private final String username;
+    private final String password;
+    private final Set<GrantedAuthority> authorities;
+
+    public DatabaseUserDetailsService(User user) {
+        this.id = user.getId();
+        this.username = user.getUsername();
+        this.password = user.getPassword();
+        this.authorities = new HashSet<GrantedAuthority>();
+
+        for (Role role : user.getRoles()) {
+            authorities.add(new SimpleGrantedAuthority(role.getName()));
+        }
+    }
+
+    public Integer getId() {
+        return this.id;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public Set<GrantedAuthority> getAuthorities() {
+        return this.authorities;
+    }
+}
