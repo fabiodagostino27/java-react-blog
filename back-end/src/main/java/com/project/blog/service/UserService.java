4ed45c3289150599d67e1a7317f0a8718c842f3b
fabiodagostino27 <fabiodago27@gmail.com>
2025-09-18 15:22:39 +0200

    ADD: saveUsermethod in userService

--- back-end/src/main/java/com/project/blog/service/UserService.java
+++ back-end/src/main/java/com/project/blog/service/UserService.java
@@ -1,11 +1,17 @@
 package com.project.blog.service;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import com.project.blog.dto.UserRegistrationDto;
+import com.project.blog.model.Role;
 import com.project.blog.model.User;
 import com.project.blog.repo.UserRepository;
+import com.project.blog.repo.RoleRepository;
 
 @Service
 public class UserService {
@@ -13,9 +19,19 @@ public class UserService {
     private UserRepository userRepository;
 
     @Autowired
+    private RoleRepository roleRepository;
+
+    @Autowired
     private PasswordEncoder passwordEncoder;
 
-    public User register() {
-        
+    public User saveUser(UserRegistrationDto userRegistrationDto) {
+        User user = new User();
+        user.setUsername(userRegistrationDto.getUsername());
+        user.setPassword(passwordEncoder.encode(userRegistrationDto.getPassword()));
+
+        Set<Role> roles = new HashSet<>();
+        roles.add(roleRepository.findByName("USER"));
+        user.setRoles(roles);
+        return userRepository.save(user);
     }
 }
