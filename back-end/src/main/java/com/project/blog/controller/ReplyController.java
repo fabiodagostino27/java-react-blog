fe01b8b7d2c25868614f04ea88ab5f5eb58dbae7
fabiodagostino27 <fabiodago27@gmail.com>
2025-09-29 16:15:00 +0200

    ADD: route to create reply

--- back-end/src/main/java/com/project/blog/controller/ReplyController.java
+++ back-end/src/main/java/com/project/blog/controller/ReplyController.java
@@ -4,35 +4,35 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.project.blog.dto.CommentCreationDTO;
-import com.project.blog.service.CommentService;
+import com.project.blog.dto.ReplyCreationDTO;
+import com.project.blog.service.ReplyService;
 
 import jakarta.validation.Valid;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 
 @RestController
 @CrossOrigin
-@RequestMapping("/api/comments")
-public class CommentController {
+@RequestMapping("/api/replies")
+public class ReplyController {
     @Autowired
-    private CommentService commentService;
+    private ReplyService replyService;
 
     @PostMapping("/{id}/create")
-    public ResponseEntity<?> create(@PathVariable Integer id, @Valid @RequestBody CommentCreationDTO commentCreationDTO, BindingResult bindingResult) {
+    public ResponseEntity<?> create(@PathVariable Integer id, @Valid @RequestBody ReplyCreationDTO replyCreationDTO, BindingResult bindingResult) {
         if (bindingResult.hasErrors()) return new ResponseEntity<>(bindingResult.getAllErrors(), HttpStatus.BAD_REQUEST);
-        
+
         try {
-            commentService.create(id, commentCreationDTO);
-            return new ResponseEntity<>("Commento creato con successo", HttpStatus.CREATED);
-        } catch (RuntimeException e) {
+            replyService.create(id, replyCreationDTO);
+            return new ResponseEntity<>("Risposta aggiunta al commento con successo!", HttpStatus.CREATED);
+        } catch (Exception e) {
             return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
         }
     }
--- back-end/src/main/java/com/project/blog/service/ReplyService.java
+++ back-end/src/main/java/com/project/blog/service/ReplyService.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
 
 import com.project.blog.dto.ReplyCreationDTO;
 import com.project.blog.model.Comment;
@@ -14,6 +15,7 @@ import com.project.blog.repo.CommentRepository;
 import com.project.blog.repo.ReplyRepository;
 import com.project.blog.repo.UserRepository;
 
+@Service
 public class ReplyService {
     @Autowired
     private ReplyRepository replyRepository;
