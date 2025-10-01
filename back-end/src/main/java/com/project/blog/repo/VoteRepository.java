adab20de1421896069109a693a8252edc9031877
fabiodagostino27 <fabiodago27@gmail.com>
2025-10-01 15:54:29 +0200

    REFINE: update VoteRepository to include methods for finding votes by user and associated entities (Post, Comment, Reply)

--- back-end/src/main/java/com/project/blog/repo/VoteRepository.java
+++ back-end/src/main/java/com/project/blog/repo/VoteRepository.java
@@ -4,8 +4,14 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.project.blog.model.Comment;
+import com.project.blog.model.Post;
+import com.project.blog.model.Reply;
+import com.project.blog.model.User;
 import com.project.blog.model.Vote;
 
 public interface VoteRepository extends JpaRepository<Vote, Integer> {
-    Optional<Vote> findByUserIdAndEntityIdAndEntityType(Integer userId, Integer entityId, String entityType);
+    Optional<Vote> findByUserAndPost(User user, Post post);
+    Optional<Vote> findByUserAndComment(User user, Comment comment);
+    Optional<Vote> findByUserAndReply(User user, Reply reply);
 }
