a2e335f7d57f9b1334db66d4024830e7e9969c7f
fabiodagostino27 <fabiodago27@gmail.com>
2025-10-07 16:15:58 +0200

    ADD: implement show method in TagController to retrieve Tag by ID

--- back-end/src/main/java/com/project/blog/controller/TagController.java
+++ back-end/src/main/java/com/project/blog/controller/TagController.java
@@ -1,12 +1,32 @@
 package com.project.blog.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.project.blog.service.TagService;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
 
 @RestController
 @CrossOrigin
 @RequestMapping("/api/tags")
 public class TagController {
+    @Autowired
+    private TagService tagService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> show(@PathVariable Integer id) {
+        try {
+            return new ResponseEntity<>(tagService.findById(id), HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
     
 }
