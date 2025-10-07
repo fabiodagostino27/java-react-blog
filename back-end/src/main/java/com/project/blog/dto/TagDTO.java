6a629dcaca65d45229c1ab57d2b42a900e5809e4
fabiodagostino27 <fabiodago27@gmail.com>
2025-10-07 16:07:59 +0200

    ADD: enhance TagDTO to initialize posts list from Tag model and include necessary getters and setters

--- back-end/src/main/java/com/project/blog/dto/TagDTO.java
+++ back-end/src/main/java/com/project/blog/dto/TagDTO.java
@@ -1,10 +1,58 @@
 package com.project.blog.dto;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import com.project.blog.model.Post;
+import com.project.blog.model.Tag;
 
 public class TagDTO {
     private Integer id;
     private String name;
     private String color;
-    private List
+    private List<SimplePostDTO> posts;
+
+    public TagDTO(Tag tag) {
+        this.id = tag.getId();
+        this.name = tag.getName();
+        this.color = tag.getColor();
+        this.posts = new ArrayList<>();
+        
+        for (Post post : tag.getPosts()) {
+            posts.add(new SimplePostDTO(post));
+        }
+    }
+
+    public Integer getId() {
+        return this.id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return this.color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public List<SimplePostDTO> getPosts() {
+        return this.posts;
+    }
+
+    public void setPosts(List<SimplePostDTO> posts) {
+        this.posts = posts;
+    }
+
 }
