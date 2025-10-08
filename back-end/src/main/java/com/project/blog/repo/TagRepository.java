00d5eb16bcc20fb0c49cc7b1f3112b928d2691db
fabiodagostino27 <fabiodago27@gmail.com>
2025-10-08 16:09:25 +0200

    ADD: import List and ensure findByName method is defined in TagRepository

--- back-end/src/main/java/com/project/blog/repo/TagRepository.java
+++ back-end/src/main/java/com/project/blog/repo/TagRepository.java
@@ -1,9 +1,11 @@
 package com.project.blog.repo;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.project.blog.model.Tag;
 
 public interface TagRepository extends JpaRepository<Tag, Integer> {
-
+    List<Tag> findByName(String name);
 }
