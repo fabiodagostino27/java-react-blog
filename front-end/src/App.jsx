f658aa4b3a2a5bb49b1b5c00ddfd08cfe5e944f9
fabiodagostino27 <fabiodago27@gmail.com>
2025-10-13 15:44:33 +0200

    ADD: wrap application in BrowserRouter and GlobalProvider for routing and state management

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,9 +1,16 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { GlobalProvider } from "./contexts/GlobalContext";
 
 function App() {
     return (
         <>
-            
+            <BrowserRouter>
+                <GlobalProvider>
+                    <Routes>
+                        
+                    </Routes>
+                </GlobalProvider>
+            </BrowserRouter>
         </>
     )
 }
