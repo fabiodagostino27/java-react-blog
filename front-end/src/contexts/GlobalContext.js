36efccdcc854226cabaedf7604a0a80c06c4271f
fabiodagostino27 <fabiodago27@gmail.com>
2025-10-13 15:40:21 +0200

    ADD: implement GlobalContext for managing global state and API URL

---
+++ front-end/src/contexts/GlobalContext.js
@@ -0,0 +1,18 @@
+import { createContext, useContext } from "react";
+
+const GlobalContext = createContext();
+
+export const GlobalProvider = ({children}) => {
+    const apiUrl = process.env.VITE_API_BASE_URL;
+
+    return (
+        <GlobalContext.Provider value={apiUrl}>
+            {children}
+        </GlobalContext.Provider>
+    )
+}
+
+export const useGlobalContext = () => useContext(GlobalContext);
+
+
+
