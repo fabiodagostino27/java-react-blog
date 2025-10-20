e7ccba4fc37a810064a018244ffde30fa614f949
fabiodagostino27 <fabiodago27@gmail.com>
2025-10-20 15:03:52 +0200

    REFINE: add HomePage component and Navbar for improved navigation and branding

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { GlobalProvider } from "./contexts/GlobalContext";
 import LoginForm from "./components/LoginForm";
 import RegisterForm from "./components/RegisterForm";
+import HomePage from "./pages/HomePage";
 
 function App() {
     return (
@@ -9,6 +10,7 @@ function App() {
             <BrowserRouter>
                 <GlobalProvider>
                     <Routes>
+                        <Route path="/" Component={HomePage} />
                         <Route path="login" Component={LoginForm} />
                         <Route path="register" Component={RegisterForm} />
                     </Routes>
---
+++ front-end/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import logo from "../assets/logo.png"
+
+export default function Navbar() {
+    return (
+        <nav class="navbar bg-body-tertiary">
+            <div class="container">
+                <a class="navbar-brand" href="#">
+                    <img src={logo} alt="Tellit" width="80px" />
+                </a>
+            </div>
+        </nav>
+    )
+}
