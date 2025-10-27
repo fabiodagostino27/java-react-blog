4c6779730848db95bd6acc5327b088b814836fc8
fabiodagostino27 <fabiodago27@gmail.com>
2025-10-27 16:24:54 +0100

    REFINE: update Navbar styling and create DefaultLayout component for consistent layout structure

--- front-end/src/components/Navbar.jsx
+++ front-end/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import logo from "../assets/logo.png"
 
 export default function Navbar() {
     return (
-        <nav class="navbar bg-body-tertiary">
+        <nav class="navbar bg-body-secondary">
             <div class="container">
                 <a class="navbar-brand" href="#">
                     <img src={logo} alt="Tellit" width="80px" />
