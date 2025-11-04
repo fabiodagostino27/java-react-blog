d894ed2935992c70b904b0f86e9839c2646c1ee3
fabiodagostino27 <fabiodago27@gmail.com>
2025-11-04 12:52:10 +0100

    REFINE: update indexRecent method to use Pageable sorting and remove unnecessary request parameter

--- back-end/src/main/java/com/project/blog/controller/PostController.java
+++ back-end/src/main/java/com/project/blog/controller/PostController.java
@@ -2,6 +2,7 @@ package com.project.blog.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,10 +21,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-
-
-
 
 @RestController
 @CrossOrigin
@@ -40,21 +37,33 @@ public class PostController {
             return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
         }
     }
-    
+
     @GetMapping
-    public ResponseEntity<Page<SimplePostDTO>> indexRecent(Pageable pageable, @RequestParam String sort) {
-        if (sort.equals("") || sort.equals("desc")) {
+    public ResponseEntity<Page<SimplePostDTO>> indexRecent(Pageable pageable) {
+        Sort sort = pageable.getSort();
+        boolean isDateDesc = false;
+
+        if (sort.isSorted()) {
+            for (Sort.Order order : sort) {
+                if (order.getProperty().equals("createdAt") && order.getDirection().isDescending()) {
+                    isDateDesc = true;
+                    break;
+                }
+            }
+        }
+
+        if (sort.isUnsorted() || isDateDesc) {
             return new ResponseEntity<>(postService.findNewest(pageable), HttpStatus.OK);
         } else {
             return new ResponseEntity<>(postService.findOldest(pageable), HttpStatus.OK);
         }
     }
-    
 
     @PostMapping("/create")
     public ResponseEntity<?> create(@Valid @RequestBody PostCreationDTO postCreationDTO, BindingResult bindingResult) {
-        if (bindingResult.hasErrors()) return new ResponseEntity<>(bindingResult.getAllErrors(), HttpStatus.BAD_REQUEST);
-        
+        if (bindingResult.hasErrors())
+            return new ResponseEntity<>(bindingResult.getAllErrors(), HttpStatus.BAD_REQUEST);
+
         try {
             postService.create(postCreationDTO);
             return new ResponseEntity<>("Post creato con successo", HttpStatus.CREATED);
