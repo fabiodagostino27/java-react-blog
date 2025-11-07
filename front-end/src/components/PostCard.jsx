4665040510c9a3e81b488effa0303484dbe802a2
fabiodagostino27 <fabiodago27@gmail.com>
2025-11-07 16:16:00 +0100

    REFINE: enhance PostCard component styling and restructure interaction buttons for better layout

--- front-end/src/components/PostCard.jsx
+++ front-end/src/components/PostCard.jsx
@@ -1,6 +1,6 @@
 export default function PostCard({ post }) {
     return (
-        <div className="card mb-3" style={{ maxWidth: "580px" }}>
+        <div className="card border-light text-bg-light mb-3" style={{ maxWidth: "580px" }}>
             <div className="row g-0">
                 <div className="col-md-4">
                     <img src={post.imagePath} className="img-fluid rounded-start" />
@@ -10,15 +10,17 @@ export default function PostCard({ post }) {
                         <span>{post.user.username}</span>
                         <h5 className="card-title">{post.title}</h5>
                         <p className="card-text"><small className="text-body-secondary">Last updated 3 mins ago</small></p>
-                        <span className="btn btn-primary align-middle">
-                            <i class="bi bi-arrow-up-circle"></i>
-                            {post.score}
-                            <i class="bi bi-arrow-down-circle"></i>
-                        </span>
-                        <span className="btn btn-primary align-middle">
-                            <i class="bi bi-chat-left"></i>
-                            {post.commentsCount}
-                        </span>
+                        <div className="d-flex gap-2">
+                            <span className="bg-info p-0 rounded-pill d-flex align-items-center gap-1 flex-shrink-1">
+                                <button className="btn btn-info rounded-circle px-2"><i class="bi bi-arrow-up-circle"></i></button>
+                                <span>{post.score}</span>
+                                <button className="btn btn-info rounded-circle px-2"><i class="bi bi-arrow-down-circle"></i></button>
+                            </span>
+                            <span className="btn btn-info rounded-pill d-flex align-items-center gap-1 flex-shrink-1">
+                                <i class="bi bi-chat"></i>
+                                {post.commentsCount}
+                            </span>
+                        </div>
                     </div>
                 </div>
             </div>
